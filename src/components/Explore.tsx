4d5562140e8cbe85aec09e56173b98b33d2ba6aa
Ashutosh-furation <Ashutosh.lakshakar@furation.tech>
2023-08-17 17:55:58 +0530

    explore page done

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -4,61 +4,52 @@ import Image from 'next/image';
 import React from 'react'
 import insta from "../../assets/Insta play.svg"
 import  exlusive from "../../assets/Exlusive.svg"
-import legal from "../../assets/Exlusive.svg"
+import legal from "../../assets/Legal.svg"
 import safe from "../../assets/safe.svg"
+import suffleAnimation from "../../assets/animation_llf41jf4_small.gif";
+import { Exploredata } from "./data/Data.js";
+// console.log("Exploredata", Exploredata);
 
 
 const Explore: React.FC = () => {
 
   return (
     <div className="w-[100vw] h-[60vh] explore-container">
-      <div className="border-2 h-[50px] mt-4 text-center  border-green-500">
+      <div className="h-[50px] mt-4 text-center  border-green-500">
         <h1 className="text-center explore-font"> Explore Sikka Play </h1>
       </div>
 
-      <div className='w-[95vw] flex justify-between'>
-        
+      <div className="w-[95vw] flex justify-around">
+        <div className=" text-center h-[40vh] mt-5 border-white-500 p-5 flex justify-around explore-offer">
+          {Exploredata.length > 0 &&
+            Exploredata.map((data) => {
+              return (
+                <div
+                  className="text-center w-[176px] h-[153px] mt-5 border-white-500"
+                  key={data.id}
+                >
+                  <div className=" text-center  h-[153px] m-auto justify-center border-white-500">
+                    <div className="w-[100%] h-[125px] m-auto mt-1 text-center">
+                      <Image
+                        src={data.img}
+                        alt="insta"
+                        className="m-auto"
+                      />
+                    </div>
+                    <p className="offer-title">{data.Title} </p>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
 
+        <div className="text-center w-[20vw] h-[30vh] border-white-500 mt-5 border-white-500">
+         
+          <Image src={suffleAnimation} alt="explore" width={500} height={500} />
+        </div>
       </div>
 
-      <div className="border-2 text-center h-[40vh] border-white-500 gap-10 flex justify-around">
-        <div className="border-2 text-center w-[176px] h-[153px] mt-5 border-white-500">
-          <div className="border-2 text-center w-[176px] h-[153px] m-auto border-white-500">
-            <Image src={insta} alt="insta" />
-          </div>
-        </div>
-        <div className="border-2 text-center w-[176px] h-[153px] mt-5 border-white-500">
-          <div className="border-2 text-center w-[176px] h-[153px] m-auto border-white-500">
-            <Image src={insta} alt="insta" />
-          </div>
-        </div>
-        <div className="border-2 text-center w-[176px] h-[153px] mt-5 border-white-500">
-          <div className="border-2 text-center w-[176px] h-[153px] m-auto border-white-500">
-            <Image src={insta} alt="insta" />
-          </div>
-        </div>
-        <div className="border-2 text-center w-[176px] h-[153px] mt-5 border-white-500">
-          <div className="border-2 text-center w-[176px] h-[153px] m-auto border-white-500">
-            <Image src={insta} alt="insta" />
-          </div>
-        </div>
-
-        
-        <div className="border-2 text-center w-[40vw] h-[153px] mt-5 border-white-500">
-          <div className="border-2 text-center w-[176px] h-[153px] m-auto border-white-500">
-            <Image src={insta} alt="insta" />
-          </div>
-        </div>
-
-
-
-      </div>
-
-      {/* <div className="border-2 text-center w-[176px] h-[153px] border-white-500">
-        <div className="border-2 text-center w-[125px] h-[125px] border-white-500">
-          <Image src={insta} alt="insta" />
-        </div>
-      </div> */}
+     
     </div>
   );
 };
--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -2,6 +2,16 @@ import Teenpatti from "../../../assets/Teenpti.svg";
 import Marriage from "../../../assets/marrige.svg";
 import community from "../../../assets/comnity.svg";
 
+import insta from "../../../assets/Insta play.svg";
+import exlusive from "../../../assets/Exlusive.svg";
+import legal from "../../../assets/Legal.svg";
+import safe from "../../../assets/safe.svg";
+
+import instaim from "../../../assets/Device.svg";
+import exlusiveim from "../../../assets/Offer (1).svg";
+import safeim from "../../../assets/Vector (2).svg";
+import legalim from "../../../assets/Term (1).svg";
+
 export const Gamedetail = [
   {
     id: 1,
@@ -22,5 +32,35 @@ export const Gamedetail = [
     Title: "Community",
     description:
       "Join our thriving Sikka Play community and meet like-minded card game  enthusiasts.",
+  },
+];
+
+
+/** game play */
+
+export const Exploredata = [
+  {
+    id: 1,
+    avatar: insta,
+    img: instaim,
+    Title: "Insta Play on Web",
+  },
+  {
+    id: 2,
+    avatar: exlusive,
+    img: exlusiveim,
+    Title: "Exclusive Offers",
+  },
+  {
+    id: 3,
+    avatar: safe,
+    img: safeim,
+    Title: "Safe & Secure ",
+  },
+  {
+    id: 4,
+    avatar: legal,
+    img: legalim,
+    Title: "100% Legal",
   },
 ];
