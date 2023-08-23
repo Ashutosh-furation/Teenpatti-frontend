aaacdc42a895763c9bb2c68f12a7eea9918eec80
Ashutosh-furation <Ashutosh.lakshakar@furation.tech>
2023-08-23 19:01:55 +0530

    google signin done

---
+++ src/app/(auth)/google/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { UserAuth } from "@/app/context/AuthContext";
+import { useEffect, useState } from "react";
+
+const page = () => {
+  const { user, googleSignIn, logOut } = UserAuth();
+  const [loading ,SetLoading]= useState(true)
+
+  console.log("user", user);
+
+  const handleSignInWithGoogle = async () => {
+    try {
+      await googleSignIn();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+    useEffect(() => {
+      const checkAuthentication = async () => {
+        await new Promise((resolve) => setTimeout(resolve, 50));
+        SetLoading(false);
+      };
+      checkAuthentication();
+    }, [user]);
+
+
+
+
+  return (
+    <div className="flex border-2 w-[60%] m-auto border-red-700 items-center justify-center h-screen">
+      {loading ? null : !user ? (
+        <div className="bg-white p-8 rounded shadow-md">
+          <h2 className="text-xl mb-4">Sign in with Google</h2>
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded w-full"
+            onClick={handleSignInWithGoogle}
+          >
+            Sign in with Google
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h1>welcome page {user.displayName} </h1>
+          <h1 onClick={handleSignOut} className="border-2 border-green-500">
+            {" "}
+            SignOut{" "}
+          </h1>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default page;
